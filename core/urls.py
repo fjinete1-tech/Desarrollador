5f18f3d40b6e4a6007089f6ff8b2ded8c4cf00c7
Fjinete1-tech <fjinete1@gmail.com>
2026-06-25 15:16:32 -0500

    actualización react run server

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,7 @@
+import django
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 from core import settings
@@ -7,3 +9,5 @@ from core import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
